5ce52022bd33eac8be7936bb72bd5b6ae33533db
Hasan Zahidi <hasan1.zahidi@gmail.com>
2017-05-31 11:40:02 +0530

    Models Revised

--- application/models/Clubs.php
+++ application/models/Clubs.php
@@ -9,15 +9,39 @@ class Clubs extends CI_Model{
         return $query->result();
     }
     public function listAllClubs(){
-        
+        $query=$this->db->query('select * from clubs');
+        return $query->result();
     }
     public function listMyClubs($user_id,$role){
-        
+        $this->load->model('Teachers');
+        $teacher_id=$this->Teachers->getTeacher($user_id)->row()->teacher_id;
+        $this->load->model('Students');
+        $student_id=$this->Students->getStudent($user_id)->row()->student_id;
+        if($role==='teacher'){
+            $query=$this->db->query('SELECT c.club_id, c.club_name, c.logo, c.club_description, c.club_link, c.purpose, c.poc '
+                . 'FROM clubs as c '
+                . 'LEFT JOIN club_enrollment as ce ON c.club_id = ce.club_id '
+                . 'WHERE tfc=\''.$teacher_id.'\'');
+            return $query->result();
+        }
+        elseif($role==='student'){
+            $query=$this->db->query('SELECT c.club_id, c.club_name, c.logo, c.club_description, c.club_link, c.purpose, c.poc, c.tfc, ce.club_id, ce.student_id '
+                . 'FROM clubs as c '
+                . 'LEFT JOIN club_enrollment as ce ON c.club_id = ce.club_id '
+                . 'WHERE student_id=\''.$student_id.'\'');
+            return $query->result();
+        }
     }
     public function getClubInfo($id){
-        
+        $query=$this->db->query('select * from clubs where club_id=\''.$id.'\'');
+        return $query->result();
     }
     public function getClubStudents($id){
-        
+        $query=$this->db->query('SELECT u.first_name, u.last_name, u.user_id, u.dp_present, s.student_id '
+                . 'FROM users as u '
+                . 'LEFT JOIN students as s ON u.user_id = s.user_id '
+                . 'LEFT JOIN club_enrollment as ce ON s.student_id = ce.student_id '
+                . 'WHERE ce.club_id=\''.$id.'\'');
+            return $query->result();
     }
 }
--- application/models/Departments.php
+++ application/models/Departments.php
@@ -9,12 +9,33 @@ class Departments extends CI_Model{
         return $query->result()->row();
     }
     public function listAllDepartments(){
-        
+        $query=$this->db->query('select department_id, stream from departments');
+        return $query->result();
     }
     public function listMyDepartments($user_id,$role){
-        
+        $this->load->model('Teachers');
+        $teacher_id=$this->Teachers->getTeacher($user_id)->row()->teacher_id;
+        $this->load->model('Students');
+        $student_id=$this->Students->getStudent($user_id)->row()->student_id;
+        if($role==='teacher'){
+            $query=$this->db->query('SELECT d.department_id, d.stream, te.teacher_id '
+                . 'FROM departments as d '
+                . 'LEFT JOIN subjects as s ON d.department_id = s.department_id '
+                . 'LEFT JOIN teachers_enrollments as te ON s.subject_id = te.subject_id '
+                . 'WHERE teacher_id=\''.$teacher_id.'\' '
+                . 'GROUP BY d.department_id');
+            return $query->result();
+        }
+        elseif($role==='student'){
+            $query=$this->db->query('SELECT d.department_id, d.stream, se.student_id '
+                . 'FROM departments as d '
+                . 'LEFT JOIN students_enrollments as se ON d.department_id = se.department_id '
+                . 'WHERE student_id=\''.$student_id.'\'');
+            return $query->result();
+        }
     }
     public function getDeparmentInfo($id){
-        
+        $query=$this->db->query('select * from departments where department_id=\''.$id.'\'');
+        return $query->result();
     }
 }
